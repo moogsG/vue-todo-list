037e08f40ad9eca66b6ab5880240c70c23f79e6a
moogs <me@moogs.work>
2017-11-22 14:27:46 -0700

    Added edit and delete to todos

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -1,36 +1,26 @@
 <template>
   <div>
-      <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
-      <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-      <div class='ui centered card' v-for="todo in todos">
-        <div class='content'>
-          <div class='header'>
-            {{ todo.title }}
-          </div>
-          <div class='meta'>
-            {{ todo.project }}
-          </div>
-          <div class='extra content'>
-            <span class='right floated edit icon'>
-              <i class='edit icon'></i>
-            </span>
-          </div>
-        </div>
-        <div class='ui bottom attached green basic button' v-show="todo.done">
-          Completed
-        </div>
-        <div class='ui bottom attached red basic button' v-show="!todo.done">
-          Complete
-        </div>
-    </div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+   <!-- // we are now passing the data to the todo component to render the todo list -->
+    <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" :key="todo.id" v-bind:todo="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
 
+import Todo from './Todo';
+
 export default {
   props: ['todos'],
+  components: {
+    Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+  },
 };
 </script>
-<style>
-</style>
