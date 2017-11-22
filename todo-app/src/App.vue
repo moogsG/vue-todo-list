799c309c19ead7780816a81255b489ddd085358b
moogs <me@moogs.work>
2017-11-22 13:43:03 -0700

    added todo object

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <TodoList v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/TodoList';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
